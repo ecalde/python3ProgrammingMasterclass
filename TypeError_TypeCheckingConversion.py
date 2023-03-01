257ddae106a660bba64cc32a16754934704a8032
Edwin Calderon <calderonedwin3@gmail.com>
2023-02-28 19:08:41 -0600

    Adding Type Error Type Checking and Conversion Work

---
+++ TypeError_TypeCheckingConversion.py
@@ -0,0 +1,13 @@
+num_char = len(input("What is your name? "))
+
+#print(type(num_char))
+
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " characters.")
+
+a = 123
+print("Variable 'a' is of type: " + str(type(a)))
+
+b = str(123)
+print("Variable 'b' is of type: " + str(type(b)))
