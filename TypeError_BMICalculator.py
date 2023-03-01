9380e351c9ffa4aa127b5d517b2a53cf26c2616f
Edwin Calderon <calderonedwin3@gmail.com>
2023-02-28 19:40:14 -0600

    Adding Type Error BMI Calculator Work

---
+++ TypeError_BMICalculator.py
@@ -0,0 +1,6 @@
+# Input your height and weight
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# print the BMI as an int
+print(int((float(weight)/(float(height) ** 2 ) ) ) )
+
