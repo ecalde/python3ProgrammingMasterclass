0b2e8aca11b0926be1524d299b94ce8dfcbcaed1
Edwin Calderon <calderonedwin3@gmail.com>
2023-03-08 08:59:55 -0600

    Adding Randomisation Importing Random Module Work

---
+++ Randomisation_ImportingRandomModule.py
@@ -0,0 +1,7 @@
+import random
+# Assign a random number to create a random heads or tails coin flip
+randomSide = random.randint(1,2)
+if randomSide == 1:
+    print("Heads")
+else:
+    print("Tails")
