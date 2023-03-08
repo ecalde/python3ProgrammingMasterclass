db8d4b17016fa5733be710c58a2fc085953bdd4d
Edwin Calderon <calderonedwin3@gmail.com>
2023-03-08 09:43:08 -0600

    Adding Lists Nested Lists Work

---
+++ Lists_NestedLists.py
@@ -0,0 +1,6 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cheries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
