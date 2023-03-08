2deb3f5b242dd17a1a8753d96f7b5572951916ba
Edwin Calderon <calderonedwin3@gmail.com>
2023-03-08 09:46:55 -0600

    Updating Lists Nested Lists Work

--- Lists_NestedLists.py
+++ Lists_NestedLists.py
@@ -4,3 +4,10 @@ vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 dirty_dozen = [fruits, vegetables]
 
 print(dirty_dozen)
+# list on index 0 is the whole fruits list
+# list on index 1 is the whole vegetables list
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+# the second square bracket is the index inside that nested index
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
