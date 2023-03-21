fea5df324d51692e3e1bdb044df45fbb3c866360
Edwin Calderon <calderonedwin3@gmail.com>
2023-03-20 23:33:35 -0500

    Updating Functions Caesar Cipher Decryption Work

--- Function_CaesarCipher.py
+++ Function_CaesarCipher.py
@@ -4,14 +4,29 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#To Do -1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+#To Do : Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
     encrypt_text = ""
-    #To Do -2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    #To Do : Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
     for letter in plain_text:
         index = alphabet.index(letter)
         encrypt_text += alphabet[index + shift_amount]
     print(f"The encoded text is {encrypt_text}")
 
-#To Do -3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-encrypt(text, shift)
+#To Do : Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(encrypt_text, shift_amount):
+    decrypt_text = ""
+  #To Do : Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+    for letter in encrypt_text:
+        index = alphabet.index(letter)
+        decrypt_text += alphabet[index - shift_amount]
+    print(f"The decoded text is {decrypt_text}")
+
+#To Do : Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+#To Do : Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
+else:
+    print("That is not a valid choice")
